325efef82ee5c053653868d960f0386ab734a9af
zcxv321 <wannit.b@ku.th>
2019-01-11 05:29:58 +0700

    fix sensordata

--- Integration/app.js
+++ Integration/app.js
@@ -287,7 +287,8 @@ app.delete('/deleteData/:id', function (req, res) {
 
   app.post('/SensorData', function (req, res) {
      var json = req.body;
-     var payload = json.DevEUI_uplink.payload_hex;
+     console.log(json)
+     //var payload = json.DevEUI_uplink.payload_hex;
     //let payload = "0073277a0167011602687b0371011601160116048601160116011605028324";
     
     var data= decodeCayennePayload(payload)
